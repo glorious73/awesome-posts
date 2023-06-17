c133cd0eac2c4b0d7f048a737379d0fe66f70dd1
glorious73 <amjadmh73@gmail.com>
2023-06-17 21:19:31 +0300

    renamed component.css to singular

--- src/styles.js
+++ src/styles.js
@@ -1,6 +1,6 @@
 import './css/style.css';
 import './css/scrollbar.css';
-import './css/components.css';
+import './css/component.css';
 import './css/animation.css';
 import './css/utils.css';
 import 'construct-style-sheets-polyfill';
