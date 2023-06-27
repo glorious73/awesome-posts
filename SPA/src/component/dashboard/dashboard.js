9f6f5d16706845e01f0dfb7b134386db7fa676b0
glorious73 <amjadmh73@gmail.com>
2023-06-27 13:52:59 +0300

    static numbers and charts

--- SPA/src/component/dashboard/dashboard.js
+++ SPA/src/component/dashboard/dashboard.js
@@ -9,7 +9,19 @@ function renderTemplate() {
   template.innerHTML = /*html*/ `
         <div class="container">
             <h1 class="title">Dashboard</h1>
-            <div class="graph" id="graph"></div>
+            <div class="dashboard-cards">
+              <div class="card dashboard-card"><h1>32</h1><label>Posts</label></div>
+              <div class="card dashboard-card"><h1>5</h1><label>Locations</label></div>
+              <div class="card dashboard-card"><h1>10</h1><label>Users</label></div>
+            </div>
+            <div class="dashboard-charts">
+              <div class="card dashboard-chart dashboard-chart-line" id="postsperhour">
+              
+              </div>
+              <div class="card dashboard-chart dashboard-chart-pie" id="locations">
+              
+              </div>
+            </div>
         </div> 
     `;
   return template;
@@ -31,17 +43,39 @@ export class Dashboard extends HTMLElement {
   }
 
   connectedCallback() {
-    const data = [{
-      values: [19, 26, 55],
-      labels: ['Residential', 'Non-Residential', 'Utility'],
-      type: 'pie'
-    }];
-    const layout = { font: { size: 18 }, legend: { x: 1, y: 1 } };
-    const config = { responsive: true };
-    Plotly.newPlot(this.shadowRoot.querySelector("#graph"), data, layout, config);
+    this.loadPostsPerHour();
+    this.loadLocations();
   }
 
   disconnectedCallback() {
 
+  }
+
+  loadPostsPerHour() {
+    const trace1 = {
+      x:['2020-10-04', '2021-11-04', '2023-12-04'],
+      y: [90, 80, 60],
+      type: 'line'
+    };
+    const data = [trace1];
+    const layout = {
+        title: 'Plotly Line Chart',
+        font: { size: 18 },
+        showlegend: false
+    };
+    const config = { responsive: false };
+    Plotly.newPlot(this.shadowRoot.querySelector("#postsperhour"), data, layout, config);
+  }
+
+  loadLocations() {
+    const data = [{
+      values: [19, 26, 55],
+      labels: ['Residential', 'Non-Residential', 'Utility'],
+      hole: 0.4,
+      type: 'pie'
+    }];
+    const layout = { title: 'Plotly Donut Chart', font: { size: 18 }, legend: { x: 1, y: 1 } };
+    const config = { responsive: false };
+    Plotly.newPlot(this.shadowRoot.querySelector("#locations"), data, layout, config);
   }
 }
