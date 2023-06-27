a6a206723f53931c98dffdb3cb6564283ff4f1ce
glorious73 <amjadmh73@gmail.com>
2023-06-27 03:33:47 +0300

    Plotly proof of concept.

--- SPA/src/component/dashboard/dashboard.js
+++ SPA/src/component/dashboard/dashboard.js
@@ -1,4 +1,7 @@
 import styles from './dashboard.css?raw';
+import plots from '../../css/plotly.css?raw';
+
+import Plotly from 'plotly.js-basic-dist-min';
 
 function renderTemplate() {
   const template = document.createElement("template");
@@ -6,6 +9,7 @@ function renderTemplate() {
   template.innerHTML = /*html*/ `
         <div class="container">
             <h1 class="title">Dashboard</h1>
+            <div class="graph" id="graph"></div>
         </div> 
     `;
   return template;
@@ -18,17 +22,26 @@ export class Dashboard extends HTMLElement {
     const shadow = this.attachShadow({ mode: "open" });
     const template = renderTemplate();
     shadow.appendChild(template.content.cloneNode(true));
-    
+
     const stylesheet = new CSSStyleSheet();
+    const plotssheet = new CSSStyleSheet();
     stylesheet.replace(styles);
-    shadow.adoptedStyleSheets = [stylesheet];
+    plotssheet.replace(plots);
+    shadow.adoptedStyleSheets = [stylesheet, plotssheet];
   }
 
   connectedCallback() {
-    
+    const data = [{
+      values: [19, 26, 55],
+      labels: ['Residential', 'Non-Residential', 'Utility'],
+      type: 'pie'
+    }];
+    const layout = { font: { size: 18 }, legend: { x: 1, y: 1 } };
+    const config = { responsive: true };
+    Plotly.newPlot(this.shadowRoot.querySelector("#graph"), data, layout, config);
   }
 
   disconnectedCallback() {
-    
+
   }
 }
